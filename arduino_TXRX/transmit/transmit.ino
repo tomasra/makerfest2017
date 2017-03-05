c4838171ad942608484d95d7d7acc56cdb95d04a
Juozas <studentasjuozas@gmail.com>
2017-03-05 10:49:15 +0200

    Added missed led value

---
+++ arduino_TXRX/transmit/transmit.ino
@@ -0,0 +1,26 @@
+int led1 = 13
+int led2 = 12;
+int laikas = 1;
+
+void setup() {
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    }
+
+void ledas1() {
+    digitalWrite(led1, 1);
+    delay(laikas);
+    digitalWrite(led1, 0);
+    }
+                     
+void ledas2() {
+    digitalWrite(led2, 1);
+    delay(laikas);
+    digitalWrite(led2, 0);
+    }
+                                 
+void loop()  {
+    ledas1();
+    ledas2();
+    delay(laikas * 7);
+    }                                        }
